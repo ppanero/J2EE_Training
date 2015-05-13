2a15e1b46409f279d015bd5eeb8968b24c75535d
ppanero <ppanero27@gmail.com>
2015-05-13 17:06:59 +0300

    Basic web app

---
+++ src/Servlets/Servlet_Example.java
@@ -0,0 +1,16 @@
+package Servlets;
+
+import java.io.IOException;
+
+/**
+ * Created by Light on 13/05/15.
+ */
+public class Servlet_Example extends javax.servlet.http.HttpServlet {
+    protected void doPost(javax.servlet.http.HttpServletRequest request, javax.servlet.http.HttpServletResponse response) throws javax.servlet.ServletException, IOException {
+
+    }
+
+    protected void doGet(javax.servlet.http.HttpServletRequest request, javax.servlet.http.HttpServletResponse response) throws javax.servlet.ServletException, IOException {
+
+    }
+}
