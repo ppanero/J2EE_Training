b2d16deb0780303803c54e8355a359cf3c34c867
ppanero <ppanero27@gmail.com>
2015-05-13 20:11:17 +0300

    User session added to the servlet

--- src/Servlets/AdvanceServletExample.java
+++ src/Servlets/AdvanceServletExample.java
@@ -5,21 +5,30 @@ import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 import java.io.IOException;
 import java.io.PrintWriter;
 
-/**
- * Created by Light on 13/05/15.
- */
 @WebServlet(name = "AdvanceServletExample", description = "A simple example servlet", urlPatterns = {"/AdvanceServletExample"})
 public class AdvanceServletExample extends HttpServlet {
+
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         //Print in the tomcat logs
-        System.out.println("GET method called");
+        System.out.println("POST method called");
         //Show in the browser
         response.setContentType("text/html");
-        String user = request.getParameter("userName");
-        if(user == null) user = "Not called properlly";
+        String userName = request.getParameter("userName");
+        HttpSession userSession = request.getSession();
+        if(userName != null){
+            if(userName.equals("")) {
+                userName = "user name not given";
+            }
+            userSession.setAttribute("userName", userName);
+        }
+        else{
+            if(userName.equals(""))
+                userName = "user name not given";
+        }
 
         PrintWriter writer = response.getWriter();
         writer.println("<!DOCTYPE html>\n" +
@@ -29,7 +38,8 @@ public class AdvanceServletExample extends HttpServlet {
                 "    <title>Advance servlet example</title>\n" +
                 "</head>\n" +
                 "<body>\n" +
-                "<div><h3>Hello " + user + "!</h3></div>\n" +
+                "<div><h3>Hello " + userName + "! (request name)</h3></div>\n" +
+                "<div><h3>Hello " + (String)userSession.getAttribute("userName") + "! (session name)</h3></div>\n" +
                 "<div><p>You called through POST</p></div>\n" +
                 "<div><p>http://localhost:8080/Web_App_Example/AdvanceServletExample?userName=yourName</p></div>\n" +
                 "</body>\n" +
@@ -37,12 +47,21 @@ public class AdvanceServletExample extends HttpServlet {
     }
 
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-         //Print in the tomcat logs
+        //Print in the tomcat logs
         System.out.println("GET method called");
         //Show in the browser
         response.setContentType("text/html");
-        String user = request.getParameter("userName");
-        if(user == null) user = "Not called properlly";
+        String userName = request.getParameter("userName");
+        HttpSession userSession = request.getSession();
+        if(userName != null){
+            if(userName.equals("")) {
+                userName = "user name not given";
+            }
+            userSession.setAttribute("userName", userName);
+        }
+        else{
+            userName = "user name not given";
+        }
 
         PrintWriter writer = response.getWriter();
         writer.println("<!DOCTYPE html>\n" +
@@ -52,7 +71,8 @@ public class AdvanceServletExample extends HttpServlet {
                 "    <title>Advance servlet example</title>\n" +
                 "</head>\n" +
                 "<body>\n" +
-                "<div><h3>Hello " + user + "!</h3></div>\n" +
+                "<div><h3>Hello " + userName + "! (request name)</h3></div>\n" +
+                "<div><h3>Hello " + (String)userSession.getAttribute("userName") + "! (session name)</h3></div>\n" +
                 "<div><p>You called through GET</p></div>\n" +
                 "<div><p>http://localhost:8080/Web_App_Example/AdvanceServletExample?userName=yourName</p></div>\n" +
                 "</body>\n" +
